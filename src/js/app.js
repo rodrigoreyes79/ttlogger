5019faf0f858ba6f3fa476dcef9634bac6fdf822
Rodrigo Reyes <rodrigo.reyes@dreamstaffing.com>
2015-02-26 13:35:52 -0500

    Added pin feature so that old entries can be reopened if not canceled. By default, new screens will be pinned

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 var moment = require('moment');
 var fs = require('fs');
 var utf8 = require('utf8');
+var gui = require('nw.gui');
+
 var lastEntryFile = 'log/.lastEntry.txt';
+var pinFile = 'log/.pins'
 
 function getLastEntry(){
 	if(fs.existsSync(lastEntryFile)){
@@ -16,14 +19,105 @@ function writeLastEntry(data, cb){
 	fs.writeFile(lastEntryFile, data, {}, cb);
 }
 
+function pin(now){
+	var pins = [];
+	if(fs.existsSync(pinFile)){
+		pins = readJsonFile(pinFile);
+	}
+	var ts = now.unix();
+	var doPin = true;
+	for(var i = 0; i < pins.length; i++){
+		if(pins[i] == ts) {
+			doPin = false;
+			break;
+		}
+	}
+	if(doPin){
+		pins.push(ts);
+		fs.writeFile(pinFile, JSON.stringify(pins), {});
+	}
+}
+
+function unpin(now){
+	var d = $.Deferred();
+	var ts = now.unix();
+	var pins = [];
+	var newPins = [];
+	if(fs.existsSync(pinFile)){
+		pins = readJsonFile(pinFile);
+	}
+	
+	for(var i = 0; i < pins.length; i++){
+		if(pins[i] == ts) {
+			continue;
+		}
+		newPins.push(pins[i]);
+	}
+	
+	fs.writeFile(pinFile, JSON.stringify(newPins), {}, function(){
+		d.resolve();
+	});
+	
+	return d.promise();
+}
+
+function reload(now){
+	// Loading pinned ts
+	var pins = readJsonFile(pinFile);
+	
+	// Opening a window per pin
+	for(var i = 0; i < pins.length; i++){
+		var pin = pins[i];
+		if(pin != now.unix()){
+			setTimeout(function(pin){ 
+				return function(){
+					var newWin = gui.Window.open('index.html',{
+						"show": true,
+						"toolbar": false,
+						"frame": true,
+						"position": "center",
+						"width": 600,
+						"height": 400,
+						"min_width": 600,
+						"min_height": 400
+					});
+					newWin.on('document-start', function() {
+						console.log('pin: ' + pin);
+						newWin.eval(null, 'sessionStorage.setItem("nowPin", ' + pin + ')');
+					});
+				}
+			}(pin), i * 1000);
+		}
+	}
+}
+
+function readJsonFile(file){
+	var data = fs.readFileSync(file, 'utf8');
+	data = JSON.parse(data);
+	return data;
+}
+
 $(function(){
-	var now = moment();
+	console.log('Starting load');
+	var nowPin = sessionStorage.getItem("nowPin");
+	console.log('nowPin: ' + nowPin);
+	var reloading = false;
+	if(nowPin){
+		now = moment.unix(nowPin);
+		reloading = true;
+	} else {
+		now = moment();
+		pin(now);
+	}	
+	
 	var fmtNow = now.format("ddd, h:mm A");  
 	document.title = fmtNow;
 	
 	if(!fs.existsSync('log')) fs.mkdirSync('log');
 	
-	var lastEntry = getLastEntry();
+	var lastEntry = null;
+	if(!reloading)
+		lastEntry = getLastEntry();
 	if(lastEntry) {
 		$('#data').val(lastEntry);
 	}
@@ -35,7 +129,11 @@ $(function(){
 		$('#data').val(lastEntry);
 	});
 	$('#cancelBtn').click(function(){
-		window.close();
+		unpin(now).done(function(){
+			var win = gui.Window.get();
+			win.hide();
+			setTimeout(win.close, 3000);
+		});
 	});
 	$('#saveBtn').click(function(){
 		writeLastEntry($('#data').val(), function(){
@@ -47,5 +145,18 @@ $(function(){
 			)
 		});
 		
+	});
+	$('#pinBtn').click(function(){
+		pin(now);
+		$('#pinBtn').hide();
+		$('#unpinBtn').show();
+	});
+	$('#unpinBtn').click(function(){
+		unpin(now);
+		$('#pinBtn').show();
+		$('#unpinBtn').hide();
+	});
+	$('#reloadBtn').click(function(){
+		reload(now);
 	});
 });
